30c3cae5bfea7da9a675e608fbed719b7a3e42a3
Mike Bronner <mike@genealabs.com>
2015-08-21 21:04:01 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,22 @@
+!DOCTYPE html>
+<!--[if lt IE 7]>
+<<html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title>Bootstrap TLDR</title>
+    <meta name="description"
+          content="All Bootstrap's components in one page. Briefly presented with their own CSS selectors.">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/matstrap.css">
+    <script src="bower_components/modernizr/modernizr.js"></script>
+</head>html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+
+
 <?php
 
 const TEST = 'var';
